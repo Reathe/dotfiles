21cb12ea452c2b4c08b81bceb1b091b4664e311d
Reathe <bachourian@gmail.com>
2026-02-25 21:04:00 +0100

    add wezterm smart switcher

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -8,97 +8,50 @@ end
 config.default_prog = { "nu" }
 config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.8
-config.win32_acrylic_accent_color = "#cba6f7"
 config.win32_system_backdrop = "Acrylic"
 config.window_decorations = "RESIZE"
-config.use_dead_keys = false
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
+config.use_dead_keys = false
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
 local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+
+local act = wezterm.action
+config.keys = {
+	-- workspace_switcher
+	{ key = "S", mods = "LEADER|CTRL", action = workspace_switcher.switch_workspace() },
+	{ key = "s", mods = "LEADER|CTRL", action = workspace_switcher.switch_to_prev_workspace() },
+	-- wezterm keybinds
+	{ key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
+	{ key = "H", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Left", 5 }) },
+	{ key = "J", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Down", 5 }) },
+	{ key = "K", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "L", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Right", 5 }) },
+	{ key = "h", mods = "CTRL|SHIFT", action = act.SplitPane({ direction = "Left" }) },
+	{ key = "j", mods = "CTRL|SHIFT", action = act.SplitPane({ direction = "Down" }) },
+	{ key = "k", mods = "CTRL|SHIFT", action = act.SplitPane({ direction = "Up" }) },
+	{ key = "l", mods = "CTRL|SHIFT", action = act.SplitPane({ direction = "Right" }) },
+	{ key = "w", mods = "CTRL", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "Enter", mods = "CTRL", action = act.ToggleFullScreen },
+	{ key = "Enter", mods = "ALT|SHIFT", action = act.TogglePaneZoomState },
+}
+
 tabline.setup({
 	options = {},
 	sections = {
 		tabline_x = {
-			{ "cpu", use_pwsh = true },
-			{ "ram", use_pwsh = true },
+			-- disabled because cpu / ram takes a long time to execute which makes the workspace name update late
+			-- { "cpu", use_pwsh = true, throttle = 15 },
+			-- { "ram", use_pwsh = true, throttle = 15 },
 		},
 	},
+	extensions = { "smart_workspace_switcher" },
 })
 tabline.apply_to_config(config)
-
-local act = wezterm.action
-config.keys = {
-	{
-		key = "h",
-		mods = "ALT",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "l",
-		mods = "ALT",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "k",
-		mods = "ALT",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "j",
-		mods = "ALT",
-		action = act.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "H",
-		mods = "ALT|SHIFT",
-		action = act.AdjustPaneSize({ "Left", 5 }),
-	},
-	{
-		key = "J",
-		mods = "ALT|SHIFT",
-		action = act.AdjustPaneSize({ "Down", 5 }),
-	},
-	{ key = "K", mods = "ALT|SHIFT", action = act.AdjustPaneSize({ "Up", 5 }) },
-	{
-		key = "L",
-		mods = "ALT|SHIFT",
-		action = act.AdjustPaneSize({ "Right", 5 }),
-	},
-	{
-		key = "h",
-		mods = "CTRL|SHIFT",
-		action = act.SplitPane({ direction = "Left" }),
-	},
-	{
-		key = "j",
-		mods = "CTRL|SHIFT",
-		action = act.SplitPane({ direction = "Down" }),
-	},
-	{
-		key = "k",
-		mods = "CTRL|SHIFT",
-		action = act.SplitPane({ direction = "Up" }),
-	},
-	{
-		key = "l",
-		mods = "CTRL|SHIFT",
-		action = act.SplitPane({ direction = "Right" }),
-	},
-	{
-		key = "w",
-		mods = "CTRL",
-		action = act.CloseCurrentPane({ confirm = true }),
-	},
-	{
-		key = "Enter",
-		mods = "CTRL",
-		action = act.ToggleFullScreen,
-	},
-	{
-		key = "Enter",
-		mods = "ALT|SHIFT",
-		action = act.TogglePaneZoomState,
-	},
-}
 
 return config
