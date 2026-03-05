7300722596cac1367830975a838ad306d31249fc
Reathe <{{ .email }}>
2026-03-05 14:46:41 +0100

    picker grep show hidden

--- dot_config/nvim/lua/plugins/config_override.lua
+++ dot_config/nvim/lua/plugins/config_override.lua
@@ -8,6 +8,7 @@ return {
         sources = {
           explorer = { hidden = true },
           files = { hidden = true },
+          grep = { hidden = true },
         },
       },
       scroll = { enabled = false },
