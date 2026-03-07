996e552236bc6864b52daf0c964b9ff71631b8e8
Reathe <{{ .email }}>
2026-03-07 19:55:43 +0100

    nvim nu default shell

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,38 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.root_spec = { ".git", "cwd" }
+
+-- Set default shell
+if vim.fn.executable("nu") == 1 then
+  vim.o.shell = "nu"
+  -- WARN: disable the usage of temp files for shell commands
+  -- because Nu doesn't support `input redirection` which Neovim uses to send buffer content to a command:
+  --      `{shell_command} < {temp_file_with_selected_buffer_content}`
+  -- When set to `false` the stdin pipe will be used instead.
+  -- NOTE: some info about `shelltemp`: https://github.com/neovim/neovim/issues/1008
+  vim.o.shelltemp = false
+
+  -- string to be used to put the output of shell commands in a temp file
+  -- 1. when 'shelltemp' is `true`
+  -- 2. in the `diff-mode` (`nvim -d file1 file2`) when `diffopt` is set
+  --    to use an external diff command: `set diffopt-=internal`
+  vim.o.shellredir = "out+err> %s"
+
+  -- flags for nu:
+  -- * `--stdin`       redirect all input to -c
+  -- * `--no-newline`  do not append `\n` to stdout
+  -- * `--commands -c` execute a command
+  vim.o.shellcmdflag = "--stdin --no-newline -c"
+
+  -- disable all escaping and quoting
+  vim.o.shellxescape = ""
+  vim.o.shellxquote = ""
+  vim.o.shellquote = ""
+
+  -- string to be used with `:make` command to:
+  -- 1. save the stderr of `makeprg` in the temp file which Neovim reads using `errorformat` to populate the `quickfix` buffer
+  -- 2. show the stdout, stderr and the return_code on the screen
+  -- NOTE: `ansi strip` removes all ansi coloring from nushell errors
+  vim.o.shellpipe =
+    "| complete | update stderr { ansi strip } | tee { get stderr | save --force --raw %s } | into record"
+end
