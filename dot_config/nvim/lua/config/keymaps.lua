95739569ba42ab223881e2b961da6cbf5d9280da
Reathe <{{ .email }}>
2026-03-07 21:29:54 +0100

    nvim terminal toggle working

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- floating terminal
+---@module "snacks"
+vim.keymap.set({ "n", "t" }, "<C-_>", function()
+  Snacks.terminal.toggle()
+end, { desc = "Toggle Terminal" })
