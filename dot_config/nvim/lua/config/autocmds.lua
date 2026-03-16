c1f5c087f6d76b19b5c72138dfb938c3032ec046
Reathe <{{ .email }}>
2026-03-16 23:18:44 +0100

    nvim remove tmp shada files

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- remove shada temp files on startup
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local shada_dir = vim.fn.stdpath("data") .. "/shada"
+    for _, file in ipairs(vim.fn.glob(shada_dir .. "/*.tmp.*", false, true)) do
+      vim.fn.delete(file)
+    end
+  end,
+})
