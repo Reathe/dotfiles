4d3e51a7b8783beec18fc1ecba3d0e82a6744487
Reathe <bachourian@gmail.com>
2026-03-19 04:33:32 +0100

    nix flake overlay install unstable lazygit

--- .nix/flake.nix
+++ .nix/flake.nix
@@ -1,19 +1,20 @@
 {
-
-  description = "My first flake";
-
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, nixpkgs-unstable, ... }:
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         modules = [
-          # Import the previous configuration.nix we used,
-          # so the old configuration file still takes effect
           ./configuration.nix
+          {
+            nixpkgs.overlays = [
+              (final: prev: { inherit (nixpkgs-unstable.legacyPackages.${final.system}) lazygit; })
+            ];
+          }
         ];
       };
     };
